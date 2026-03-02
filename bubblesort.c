7c15d480ece7548fd2ab6ea13cc769d11698571b
Shweta2332006 <shwetachandrashekharhittalaman@gmail.com>
2026-03-02 15:40:35 +0530

    version 3optimized bubble sort with early exit

--- bubblesort.c
+++ bubblesort.c
@@ -1,19 +1,26 @@
 #include<stdio.h>
-void bubblesort(int arr[],int n){
+int bubblesort(int arr[],int n){
   for(int i=0;i<n-1;i++){
+    int swapped=0;
     for(int j=0;j<n-i-1;j++){
       if(arr[j]>arr[j+1]){
         int temp=arr[j];
         arr[j]=arr[j+1];
         arr[j+1]=temp;
+        swapped=1;
       }
     }
   }
+  return swapped;
 }
 int main(){
   int arr[]={5,3,8,4,2};
   int n=5;
-  bubblesort(arr,n);
+  int swap=bubblesort(arr,n);
+  if(swap==0){
+    printf("array is already sorted\n");
+    break;
+  }
   printf("Sorted array:");
   for(int i=0;i<n;i++){
     printf("%d",arr[i]);
